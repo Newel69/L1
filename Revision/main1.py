c0bdf8b1651611f20bdb36fafd168597496aa083
Newel69 <raphynewton@gmail.com>
2024-11-04 17:54:15 +0100

    Add files via upload

---
+++ Revision/main1.py
@@ -0,0 +1,16 @@
+print("My name is Rahael.")
+book="Lord of the rings"
+print ("My favorite book is",book,".")
+weight=30
+print ("My weight is",weight,"Kg.")
+ice_cream=input("What is your favorite ice cream flavor?")
+print (ice_cream)
+age=int(input("How old are you?"))
+print ("I am",age,"years old.")
+name=input("What is your name?")
+for i in range (2):
+    print ("Happy birthday to you")
+print ("Happy birthday dear",name)
+print ("Happy birthday to you")
+for i in range (15,31):
+    print (i)
